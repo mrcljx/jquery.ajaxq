23d773fef3af752e8a3b63f5c3034f74a3c28c2c
Marcel Jackwerth <marcel@northdocks.com>
2011-03-21 15:38:43 +0100

    convert tabs to spaces / remove whitespace

--- jquery.ajaxq.js
+++ jquery.ajaxq.js
@@ -13,51 +13,51 @@
 
 jQuery.ajaxq = function (queue, options)
 {
-	// Initialize storage for request queues if it's not initialized yet
-	if (typeof document.ajaxq == "undefined") document.ajaxq = {q:{}, r:null};
+  // Initialize storage for request queues if it's not initialized yet
+  if (typeof document.ajaxq == "undefined") document.ajaxq = {q:{}, r:null};
 
-	// Initialize current queue if it's not initialized yet
-	if (typeof document.ajaxq.q[queue] == "undefined") document.ajaxq.q[queue] = [];
-	
-	if (typeof options != "undefined") // Request settings are given, enqueue the new request
-	{
-		// Copy the original options, because options.complete is going to be overridden
+  // Initialize current queue if it's not initialized yet
+  if (typeof document.ajaxq.q[queue] == "undefined") document.ajaxq.q[queue] = [];
+  
+  if (typeof options != "undefined") // Request settings are given, enqueue the new request
+  {
+    // Copy the original options, because options.complete is going to be overridden
 
-		var optionsCopy = {};
-		for (var o in options) optionsCopy[o] = options[o];
-		options = optionsCopy;
-		
-		// Override the original callback
+    var optionsCopy = {};
+    for (var o in options) optionsCopy[o] = options[o];
+    options = optionsCopy;
+    
+    // Override the original callback
 
-		var originalCompleteCallback = options.complete;
+    var originalCompleteCallback = options.complete;
 
-		options.complete = function (request, status)
-		{
-			// Dequeue the current request
-			document.ajaxq.q[queue].shift ();
-			document.ajaxq.r = null;
-			
-			// Run the original callback
-			if (originalCompleteCallback) originalCompleteCallback (request, status);
+    options.complete = function (request, status)
+    {
+      // Dequeue the current request
+      document.ajaxq.q[queue].shift ();
+      document.ajaxq.r = null;
+      
+      // Run the original callback
+      if (originalCompleteCallback) originalCompleteCallback (request, status);
 
-			// Run the next request from the queue
-			if (document.ajaxq.q[queue].length > 0) document.ajaxq.r = jQuery.ajax (document.ajaxq.q[queue][0]);
-		};
+      // Run the next request from the queue
+      if (document.ajaxq.q[queue].length > 0) document.ajaxq.r = jQuery.ajax (document.ajaxq.q[queue][0]);
+    };
 
-		// Enqueue the request
-		document.ajaxq.q[queue].push (options);
+    // Enqueue the request
+    document.ajaxq.q[queue].push (options);
 
-		// Also, if no request is currently running, start it
-		if (document.ajaxq.q[queue].length == 1) document.ajaxq.r = jQuery.ajax (options);
-	}
-	else // No request settings are given, stop current request and clear the queue
-	{
-		if (document.ajaxq.r)
-		{
-			document.ajaxq.r.abort ();
-			document.ajaxq.r = null;
-		}
+    // Also, if no request is currently running, start it
+    if (document.ajaxq.q[queue].length == 1) document.ajaxq.r = jQuery.ajax (options);
+  }
+  else // No request settings are given, stop current request and clear the queue
+  {
+    if (document.ajaxq.r)
+    {
+      document.ajaxq.r.abort ();
+      document.ajaxq.r = null;
+    }
 
-		document.ajaxq.q[queue] = [];
-	}
+    document.ajaxq.q[queue] = [];
+  }
 }
